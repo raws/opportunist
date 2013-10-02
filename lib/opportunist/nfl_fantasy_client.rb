bf2092e2db44db2010a86ed9436488c6a4003bfb
Ross Paffett <ross@rosspaffett.com>
2013-10-01 23:14:52 -0400

    Add NflFantasyClient#available_players

--- lib/opportunist/nfl_fantasy_client.rb
+++ lib/opportunist/nfl_fantasy_client.rb
@@ -1,14 +1,40 @@
 module Opportunist
   class NflFantasyClient
     SIGN_IN_URL = 'https://id2.s.nfl.com/fans/login'
+    PLAYERS_BASE_URL = 'http://fantasy.nfl.com/league/%{id}/players?playerStatus=available'
+    PLAYERS_URLS = {
+      defense: "#{PLAYERS_BASE_URL}&position=8",
+      kicker: "#{PLAYERS_BASE_URL}&position=7",
+      quarterback: "#{PLAYERS_BASE_URL}&position=1",
+      running_back: "#{PLAYERS_BASE_URL}&position=2",
+      tight_end: "#{PLAYERS_BASE_URL}&position=4",
+      wide_receiver: "#{PLAYERS_BASE_URL}&position=3"
+    }
 
-    def initialize(username, password)
+    def initialize(username, password, league_id, position)
       @username = username
       @password = password
+      @league_id = league_id
+      @position = position
       @agent = Mechanize.new
     end
 
+    def available_players
+      @available_players ||= table_rows.map do |row|
+        row.at_css('td.playerNameAndInfo a.playerName').content.strip
+      end
+    end
+
     private
+
+    def page
+      if @page
+        @page
+      else
+        sign_in
+        @page = @agent.get(url)
+      end
+    end
 
     def sign_in
       page = @agent.get(SIGN_IN_URL)
@@ -16,6 +42,16 @@ module Opportunist
       form.username = @username
       form.password = @password
       @agent.submit(form)
+    end
+
+    def table_rows
+      @table_rows ||= page.search('table.tableType-player > tbody > tr').map do |row|
+        row.css('td')
+      end
+    end
+
+    def url
+      PLAYERS_URLS[@position] % { id: @league_id }
     end
   end
 end
